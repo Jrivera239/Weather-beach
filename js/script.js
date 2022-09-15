267ba00f764eb847f6b3013ef25ab04108757dff
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-15 19:07:07 -0400

    updating script

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@ var inputStorage = document.querySelector('#inputs');
 var output = document.querySelector('#output');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
+API = "c3ed635e20e129bc3f138cc61aabb462"
 
-
+function weatherTep(val)
+{
+    return (val - 273).temp(2)
+}
+    btn.addEventListener('click', function()
+{
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid='+API)
+        .then(res => res.json()).then(data => {
+            var name = data['name'];
+            var humidity = data['main'] ['humidity']  ;
+            var temp = data['main'] ['temperature'];
+            var windspeed = data['speed'] ['wind'];
