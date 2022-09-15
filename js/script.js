24bb5f045a27facf66b1958df7bdc02fbcdc7d56
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-15 17:47:43 -0400

    script vars

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//vars query
+
+var add = document.querySelector('#add');
+var info = document.querySelector('#description');
+var input = document.querySelector('#input');
+var inputStorage = document.querySelector('#inputs');
+var output = document.querySelector('#output');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+
+
