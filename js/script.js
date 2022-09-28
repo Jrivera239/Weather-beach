93b66d72809a9a5cd70bb682e13c79ff459d6595
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-28 18:14:44 -0400

    starting over Script
    My last script wasn't working so im restarting again

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 
+// joining ids selectors
+
+const today = document.querySelector("#today");
+const saveCity = document.querySelector("#saveCity");
+const fiveDayCast = document.querySelector("#fiveDayCast");
+const searchBtn = document.querySelector("#userBTN");
+let userInput = document.querySelector("#userInput");
+let city = "";
+let cityArray = [];
+let clearHistory = document.querySelector("#clearHistory");
+
+// shows today's day //
+
+const todayTemp = document.querySelector("#todayTemp");
+const todayHumid = document.querySelector("#todayHumid");
+const todayWinds = document.querySelector("#todayWind");
+const todayIcon = document.querySelector("#todayIcon");
+const todayIndex = document.querySelector("#todayIndex");
+const todayName = document.querySelector("#todayName");
+
+// five day forecast //
+
+const fiveDay = document.querySelector("#fiveDayCast");
+const forecastDays = document.querySelectorAll(".forecastDays");
+const fiveDate = document.querySelectorAll(".dateF");
+console.log(forecastDays);
+
 
 
 
