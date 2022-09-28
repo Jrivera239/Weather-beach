33b3334caa023fd2e2983b577d7f5823a4e8ebaa
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-09-28 19:44:27 -0400

    update script
    added functions, city save history, connection with HTML and openweather

--- js/script.js
+++ js/script.js
@@ -22,9 +22,9 @@ const todayName = document.querySelector("#todayName");
 // five day forecast //
 
 const fiveDay = document.querySelector("#fiveDayCast");
-const forecastDays = document.querySelectorAll(".forecastDays");
+const ForecastDays = document.querySelectorAll(".forecastDays");
 const fiveDate = document.querySelectorAll(".dateF");
-console.log(forecastDays);
+console.log(ForecastDays);
 
 var seachArray = function (city) {
     for (var i = 0; i < cityArray.length;i++) {
@@ -35,15 +35,15 @@ var seachArray = function (city) {
     return 1;
 };
 
-// calls weather API 
+// calls weather API //
 var getWeatherAPI = function (lat, lon) {
 const openweathermap = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,alerts,minutely&appid=a22fcda0659e95b539385ad289716ca4`;
 
-fetch (openweatheramp) 
+fetch (openweathermap) 
 .then ((res) => res.json())
 .then ((data) => {
     console.log(data);
-    displayFuture(data.daily);
+    displayForecastDays(data.daily);
 });
 }
 
@@ -65,7 +65,7 @@ var convertnputApi = function (city) {
   });
 };
 
-//shows data pulled for current day
+// shows current day data //
 
 var displayToday = function (today) {
   let todayWeather = today.today;
@@ -77,8 +77,55 @@ var displayToday = function (today) {
   todayWinds.innerHTML = ` ${todayWeather.wind_speed} MPH`;
   todayIndex.innerHTML = ` ${todayWeather.uvi}`;
 
-  //need image to appear instead
+  // allows image to appear //
+
   let todayIcon = `${todayWeather.weather[0].icon}`;
   let iconUrl = `https://openweathermap.org/img/wn/${todayIcon}@2x.png`;
   todayIcon.innerHTML = "<img src=" + iconUrl + ">";
 };
+
+//displays forecast for 5 days
+
+var displaCurrent = function (fiveday) {
+    for (var i = 0; i < 5; i++) {
+
+      let dailyDate = new Date(fiveday[i].dt * 1000).toLocaleDateString();
+      console.log(dailyDate);
+
+      let tempFive = fiveday[i].temp.day;
+      let dailyIcon = `${fiveday[i].weather[0].icon}`;
+      let dailyIconUrl = `https://openweathermap.org/img/wn/${dailyIcon}@2x.png`;
+  
+
+    //places temp, wind and humidity, in their respective places
+    document.querySelector(`#statustemp${[i]}`).innerHTML = " " + tempFive;
+    document.querySelector(`#statuswind${[i]}`).innerHTML =
+      " " + fiveday[i].wind_speed + " MPH";
+    document.querySelector(`#statushumid${[i]}`).innerHTML =
+      " " + fiveday[i].humidity + "%";
+    document.querySelector(`#statusimage${[i]}`).innerHTML =
+      "<img src=" + dailyIconUrl + ">";
+    document.querySelector(`#statusdate${[i]}`).innerHTML = dailyDate;
+  }
+};
+// saves the searched cities on search history //
+var saveCityName = function (city) {
+  cityArray = JSON.parse(localStorage.getItem("city"));
+  if (cityArray == null) {
+    cityArray = [];
+    cityArray.push(city.toUpperCase());
+    localStorage.setItem("city", JSON.stringify(cityArray));
+    appendToList(city.toUpperCase());
+  } else {
+    if (searchArray(city) > 0) {
+      cityArray.push(city.toUpperCase());
+      localStorage.setItem("city", JSON.stringify(cityArray));
+      appendToList(city.toUpperCase());
+    }
+  }
+  userInput.value = "";
+};
+
+
+
+
